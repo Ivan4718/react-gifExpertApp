c6f2013a2a3494a5db1bcf488272cd1c3ed952b2
Ivan4718 <ivanramirez1997@hotmail.com>
2021-12-16 13:02:40 +0100

    First commit

---
+++ src/components/GifGridItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'animate.css';
+
+export const GifGridItem = ({id , title, url}) => {
+
+    console.log (id, title, url);
+    return (
+        <div className = "card animate__animated animate__flash">
+           <img src = { url } alt = { title }/>
+           <p>{title}</p>
+        </div>
+    )
+}
